3663fc14b1038b5dffeba4ba515682c717526570
Sal Samani <sal@milysec.com>
2024-07-22 06:17:00 +1000

    ready

---
+++ src/app/api/getTokenHolders/route.js
@@ -0,0 +1,47 @@
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID, AccountLayout } from '@solana/spl-token';
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+    const { mintAddress } = await request.json();
+
+    if (!mintAddress) {
+        return NextResponse.json({ message: 'Mint address is required' }, { status: 400 });
+    }
+
+    try {
+        const connection = new Connection(process.env.RPCM);
+        const mint = new PublicKey(mintAddress);
+
+        const tokenAccounts = await connection.getProgramAccounts(
+            TOKEN_PROGRAM_ID,
+            {
+                filters: [
+                    { dataSize: 165 },
+                    {
+                        memcmp: {
+                            offset: 0,
+                            bytes: mint.toBase58(),
+                        },
+                    },
+                ],
+            }
+        );
+
+        const holders = tokenAccounts
+            .map(account => {
+                const accountData = AccountLayout.decode(account.account.data);
+                return {
+                    address: account.pubkey.toString(),
+                    balance: accountData.amount.toString(),
+                };
+            })
+        // .filter(holder => parseInt(holder.balance) > 0);
+        // .sort((a, b) => BigInt(b.balance) - BigInt(a.balance)); // Sort by balance, highest to lowest
+
+        return NextResponse.json(holders);
+    } catch (error) {
+        console.error('Error:', error);
+        return NextResponse.json({ message: 'Error fetching token holders' }, { status: 500 });
+    }
+}
